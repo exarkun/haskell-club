ab44391555b506b0cbcd86772027005ade1646e1
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2019-06-06 09:29:46 -0400

    Slightly better validate tests and a corrected implementation

--- homework/homework1/app/Main.hs
+++ homework/homework1/app/Main.hs
@@ -4,6 +4,8 @@ import Lib
     ( toDigits
     , toDigitsRev
     , doubleEveryOther
+    , doubleEveryOther'
+    , doubleEveryOther''
     , sumDigits
     , validate
     )
@@ -14,22 +16,45 @@ import Hanoi
 
 main :: IO ()
 main = do
+    putStrLn "toDigits"
     putStrLn $ show $ toDigits 0
     putStrLn $ show $ toDigits (-17)
     putStrLn $ show $ toDigits 1234
     putStrLn $ show $ toDigitsRev 1234
 
+    putStrLn "doubleEveryOther"
     putStrLn $ show $ doubleEveryOther []
     putStrLn $ show $ doubleEveryOther [1]
     putStrLn $ show $ doubleEveryOther [1,1,1]
     putStrLn $ show $ doubleEveryOther [1,1,1,1]
 
+    putStrLn "doubleEveryOther'"
+    putStrLn $ show $ doubleEveryOther' []
+    putStrLn $ show $ doubleEveryOther' [1]
+    putStrLn $ show $ doubleEveryOther' [1,1,1]
+    putStrLn $ show $ doubleEveryOther' [1,1,1,1]
+
+    putStrLn "doubleEveryOther''"
+    putStrLn $ show $ doubleEveryOther'' []
+    putStrLn $ show $ doubleEveryOther'' [1]
+    putStrLn $ show $ doubleEveryOther'' [1,1,1]
+    putStrLn $ show $ doubleEveryOther'' [1,1,1,1]
+
+    putStrLn "sumDigits"
     putStrLn $ show $ sumDigits [1, 2, 3]
     putStrLn $ show $ sumDigits [10, 2, 3]
     putStrLn $ show $ sumDigits [15, 25, 35]
 
-    putStrLn $ show $ validate 4012888888881881
-    putStrLn $ show $ validate 4012888888881882
+    putStrLn "validate True"
+    putStrLn $ show $ validate 0                  -- 0 == 0 mod 10
+    putStrLn $ show $ validate 18                 -- 0 == (1 * 2) + 8 mod 10
+    putStrLn $ show $ validate 26                 -- 0 == (2 * 2) + 6 mod 10
+    putStrLn $ show $ validate 4012888888881881   -- ...
+    putStrLn "validate False"
+    putStrLn $ show $ validate 1                  -- 0 /= 1 mod 10
+    putStrLn $ show $ validate 28                 -- 0 /= (2 * 2) + 8 mod 10
+    putStrLn $ show $ validate 4012888888881882   -- ...
 
+    putStrLn "hanoi"
     putStrLn $ show $ hanoi 2 "a" "b" "c"
     putStrLn $ show $ hanoi 5 "a" "b" "c"
--- homework/homework1/src/Lib.hs
+++ homework/homework1/src/Lib.hs
@@ -2,6 +2,8 @@ module Lib
     ( toDigits
     , toDigitsRev
     , doubleEveryOther
+    , doubleEveryOther'
+    , doubleEveryOther''
     , sumDigits
     , validate
     ) where
@@ -14,10 +16,28 @@ toDigitsRev n
     | n <= 0    = []
     | otherwise = (n `mod` 10):(toDigitsRev (n `div` 10))
 
+-- wrong implementation
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:[]) = [x]
 doubleEveryOther (x:y:zs) = x:y*2:doubleEveryOther zs
+
+-- corrected implementation
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' (x:[]) = [x]
+doubleEveryOther' many = reverse . doubleEveryOther . reverse $ many
+
+-- Less reverse-y implementation
+doubleEveryOther'' :: [Integer] -> [Integer]
+doubleEveryOther'' many
+  | even . length $ many = applyEvery (2*) id many
+  | odd . length $ many  = applyEvery id (2*) many
+  where
+    applyEvery :: (Integer -> Integer) -> (Integer -> Integer) -> [Integer] -> [Integer]
+    applyEvery _ _ []            = []
+    applyEvery _ _ (x:[])        = [x]
+    applyEvery first second (x:y:rest) = [first x, second y] ++ applyEvery first second rest
 
 sumDigits :: [Integer] -> Integer
 sumDigits [] = 0
@@ -30,4 +50,4 @@ validate =
     -- (sumDigits (doubleEveryOther (toDigitsRev n))) `mod` 10 == 0
     -- (sumDigits . doubleEveryOther . toDigitsRev) n `mod` 10 == 0
     -- ((flip mod 10) . sumDigits . doubleEveryOther . toDigitsRev) n == 0
-    ((==) 0 . (flip mod 10) . sumDigits . doubleEveryOther . toDigitsRev)
+    ((==) 0 . (flip mod 10) . sumDigits . doubleEveryOther'' . toDigits)
